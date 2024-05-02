c4647cae8322378ae3d0f9945d3000e9dab4460d
kurosakinoel <stardustdrago@gmail.com>
2024-05-02 02:41:38 -0700

    Testing custom input

--- deepl.js
+++ deepl.js
@@ -1,28 +1,40 @@
 ﻿// Import the axios library
 const axios = require('axios');
+const http = require('http');
 
-// Define the DeepL API endpoint
 const apiUrl = 'https://api-free.deepl.com/v2/translate';
-
-// Define your DeepL API key
 const deeplAuthKey = process.env.deeplAuthKey;
+var deeplRequestData = undefined;
+
+const server = http.createServer(function(request, response){
+    if(request.method == "POST"){
+        response.end('received post request');
+        deeplRequestData = request;
+    }
+    deeplTranslate(deeplRequestData);
+});
+
+//var text = ;
 
 // Define the data to be sent in the request
-const requestData = {
-    text: 'Hello, world!',
-    target_lang: 'ZH'
-};
+// const requestData = {
+//     text: 'Hello, world!',
+//     target_lang: 'ZH'
+// };
 
-// Set up the Axios POST request
-axios.post(apiUrl, requestData, {
-    headers: {
-        'Authorization': `DeepL-Auth-Key ${deeplAuthKey}`,
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
-})
-.then(response => {
-    console.log('Response:', response.data);
-})
-.catch(error => {
-    console.error('Error:', error);
-});
+function deeplTranslate(deeplRequestData, translatedData){
+    // Set up the Axios POST request
+    axios.post(apiUrl, deeplRequestData, {
+        headers: {
+            'Authorization': `DeepL-Auth-Key ${deeplAuthKey}`,
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    })
+    .then(response => {
+        console.log('Response:', response.data);
+        //console.log('Response:', translatedData);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+}
