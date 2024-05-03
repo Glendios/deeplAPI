172b4457045478ebcce8927d6dea4fce104ddae8
kurosakinoel <stardustdrago@gmail.com>
2024-05-03 10:17:19 -0700

    Reactivate CORS

--- deepl.js
+++ deepl.js
@@ -13,8 +13,8 @@ const corsOptions = {
     optionsSuccessStatus: 200
 };
 
-//app.use(cors(corsOptions));
-app.use(cors());
+app.use(cors(corsOptions));
+//app.use(cors());
 app.use(express.json());
 
 const deeplRequestData = {
